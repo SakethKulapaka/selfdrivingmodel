5c8293796271e95d7ad1b55def383a9a32369fd9
Saketh Kulapaka <sakethk98@gmail.com>
2019-05-11 11:16:02 +0530

    driving car with remote control and collecting data

--- remotecontrol.py
+++ remotecontrol.py
@@ -20,6 +20,7 @@ en2 = 26
 #servo 5V pin 2 GND pin 9
 #motor driver 5v pin4 GND pin 6
 
+#intializing GPIO pins
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servo_control, GPIO.OUT)
@@ -29,12 +30,14 @@ GPIO.setup(motortwo_1, GPIO.OUT)
 GPIO.setup(motortwo_2, GPIO.OUT)
 GPIO.setup(en1, GPIO.OUT)
 GPIO.setup(en2, GPIO.OUT)
+
+#initializing dc motors
 p1 = GPIO.PWM(en1, 100)
 p2 = GPIO.PWM(en2, 100)
 p1.start(30)
 p2.start(30)
 
-#input pins
+#input remote control pins
 left = 10
 up = 9
 right = 11
@@ -43,10 +46,12 @@ GPIO.setup(up, GPIO.IN)
 GPIO.setup(left, GPIO.IN)
 GPIO.setup(right, GPIO.IN)
 
+#initializing the servo motor
 p = GPIO.PWM(servo_control, 100)
 p.start(0)
 p.ChangeDutyCycle(angle)
 
+#intializing pi camera
 vs = VideoStream(usePiCamera = True, resolution = (480,360)).start()
 time.sleep(2.0)
 
@@ -54,6 +59,7 @@ i=0
 img = []
 ang = []
 output = []
+#create folder to save data
 folder = input("Enter folder name : ")
 os.mkdir("/home/pi/tensorflow-env/Project/" + folder)
 op = ''
@@ -61,11 +67,13 @@ op = ''
 try :
     
     while True :
+        #read and display video frame
         image = vs.read()
         cv2.imshow('live',image)
 
         save = 0
         
+        #move forward if button is pressed
         if(GPIO.input(up)) :
             op = 'up'                         
             GPIO.output((motorone_1,motortwo_1),0)
@@ -73,7 +81,8 @@ try :
             time.sleep(0.2)
             GPIO.output((motorone_2,motortwo_2),0)
             save = 1
-
+            
+        #turn left if button is pressed
         if(GPIO.input(left)) :
             save = 1
             while(GPIO.input(left)) :
@@ -84,7 +93,8 @@ try :
                     angle=9
                 p.ChangeDutyCycle(angle)
             p.ChangeDutyCycle(0)
-
+            
+        #turn right if button is pressed
         if(GPIO.input(right)) :
             save = 1
             while(GPIO.input(right)) :
@@ -96,7 +106,8 @@ try :
                 p.ChangeDutyCycle(angle)
             p.ChangeDutyCycle(0)
         print(op)
-
+        
+        #add data to list when button is pressed
         if(save==1) :
             cv2.imwrite(str(folder) + '/' + str(i) + '.jpg', image)
             i = i+1
@@ -112,6 +123,7 @@ try :
 except KeyboardInterrupt :
     GPIO.cleanup()
 
+#turn off GPIO and camera
 GPIO.cleanup()
 cv2.destroyAllWindows()
 vs.stop()
@@ -127,6 +139,7 @@ print("Processing\n")
 
 temp = np.concatenate((image,angle,output), axis = 1)
 
+#save the data as a csv file
 print("writing to csv")
 df = pd.DataFrame(temp,columns = ['Image', 'Angle', 'ouptut'])
 df.to_csv('dataset_' + str(folder) + '.csv')

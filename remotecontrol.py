f36ddbc3966f979640f5a950a4cafc069bc2b281
Saketh Kulapaka <35227428+SakethKulapaka@users.noreply.github.com>
2019-05-11 10:36:32 +0530

    driving with a remote control and collecting data

---
+++ remotecontrol.py
@@ -0,0 +1,134 @@
+from imutils.video import VideoStream
+import cv2
+import numpy as np
+import time
+import pandas as pd
+import RPi.GPIO as GPIO
+import os
+
+save =0
+angle = 11
+        
+#output pins
+servo_control = 13
+motorone_1 = 17
+motorone_2 = 27
+motortwo_1 = 22
+motortwo_2 = 23
+en1 = 19
+en2 = 26
+#servo 5V pin 2 GND pin 9
+#motor driver 5v pin4 GND pin 6
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servo_control, GPIO.OUT)
+GPIO.setup(motorone_1, GPIO.OUT)
+GPIO.setup(motorone_2, GPIO.OUT)
+GPIO.setup(motortwo_1, GPIO.OUT)
+GPIO.setup(motortwo_2, GPIO.OUT)
+GPIO.setup(en1, GPIO.OUT)
+GPIO.setup(en2, GPIO.OUT)
+p1 = GPIO.PWM(en1, 100)
+p2 = GPIO.PWM(en2, 100)
+p1.start(30)
+p2.start(30)
+
+#input pins
+left = 10
+up = 9
+right = 11
+
+GPIO.setup(up, GPIO.IN)
+GPIO.setup(left, GPIO.IN)
+GPIO.setup(right, GPIO.IN)
+
+p = GPIO.PWM(servo_control, 100)
+p.start(0)
+p.ChangeDutyCycle(angle)
+
+vs = VideoStream(usePiCamera = True, resolution = (480,360)).start()
+time.sleep(2.0)
+
+i=0
+img = []
+ang = []
+output = []
+folder = input("Enter folder name : ")
+os.mkdir("/home/pi/tensorflow-env/Project/" + folder)
+op = ''
+
+try :
+    
+    while True :
+        image = vs.read()
+        cv2.imshow('live',image)
+
+        save = 0
+        
+        if(GPIO.input(up)) :
+            op = 'up'                         
+            GPIO.output((motorone_1,motortwo_1),0)
+            GPIO.output((motorone_2,motortwo_2),1)
+            time.sleep(0.2)
+            GPIO.output((motorone_2,motortwo_2),0)
+            save = 1
+
+        if(GPIO.input(left)) :
+            save = 1
+            while(GPIO.input(left)) :
+                op = 'left'
+                angle = angle - 0.000005
+                if(angle<=9) :
+                    p.ChangeDutyCycle(0)
+                    angle=9
+                p.ChangeDutyCycle(angle)
+            p.ChangeDutyCycle(0)
+
+        if(GPIO.input(right)) :
+            save = 1
+            while(GPIO.input(right)) :
+                op = 'right'
+                angle = angle + 0.000005
+                if(angle>=12.5) :
+                    p.ChangeDutyCycle(0)
+                    angle=12.5
+                p.ChangeDutyCycle(angle)
+            p.ChangeDutyCycle(0)
+        print(op)
+
+        if(save==1) :
+            cv2.imwrite(str(folder) + '/' + str(i) + '.jpg', image)
+            i = i+1
+            img.append(str(folder) + '/' + str(i) + '.jpg')
+            ang.append(angle)
+            output.append(op)
+
+        key = cv2.waitKey(1) & 0xFF
+        if key==ord('q') :
+            break
+        
+        
+except KeyboardInterrupt :
+    GPIO.cleanup()
+
+GPIO.cleanup()
+cv2.destroyAllWindows()
+vs.stop()
+print("camera off")
+
+image = np.array(img)
+image = image.reshape(image.shape[0],1)
+angle = np.array(ang)
+angle = angle.reshape(angle.shape[0],1)
+output = np.array(output)
+output = output.reshape(output.shape[0],1)
+print("Processing\n")
+
+temp = np.concatenate((image,angle,output), axis = 1)
+
+print("writing to csv")
+df = pd.DataFrame(temp,columns = ['Image', 'Angle', 'ouptut'])
+df.to_csv('dataset_' + str(folder) + '.csv')
+print("done")
+
